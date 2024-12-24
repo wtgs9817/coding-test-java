9caecc28f80d0dd34951ad22d14b16e0780bc9e2
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2024-12-24 14:34:19 +0900

    Update 약수의 합.java

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int n) {
-        if(n < 0 && n > 3000) {
-            return 0;
-        }
         int answer = 0;
         
         for(int i=1; i<=n/2; i++) {
