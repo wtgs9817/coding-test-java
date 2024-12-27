bd5c70a1e305d69b780412b11c13b62e0085f5a5
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2024-12-27 17:25:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,8 +1,27 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        int len = s.length();
         
+        if(s.length() == 4 || s.length() == 6) {
+            for(char c : s.toCharArray()) {
+                if(!Character.isDigit(c)) {
+                    answer = false;
+                }
+                
+            }                
+        }
+        
+        else {
+            answer = false;
+        }
+        
+        
+        
+        
+        
+        
+        
+        /*  예외는 비정상적인 상황에서만 사용해야한다. 이런식으로 해결하는 건 지양해야 함.
         if(len == 4 || len == 6) {
                    
         try {
@@ -18,7 +37,9 @@ class Solution {
             answer = false;
             
         }
-           
+           */
+        
+        
         
         return answer;
     }
