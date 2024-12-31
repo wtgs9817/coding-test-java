f63e2fe5235fac78484c8191784f577011b5dac0
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2024-12-31 15:25:39 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.25 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        String answer ="";
+        String[] arr = s.split(" ", -1);
+       
+        for(int i=0; i<arr.length; i++) {
+            
+            for(int k=0; k<arr[i].length(); k++) {
+                if(k % 2 ==0) {
+                    answer += String.valueOf(arr[i].charAt(k)).toUpperCase();
+                }
+                else {
+                    answer += String.valueOf(arr[i].charAt(k)).toLowerCase();        
+                }
+            }
+            
+            if(i != arr.length-1) {
+                answer+=" ";
+            }
+            
+            
+        }
+         
+        return answer;
+    }
+}
