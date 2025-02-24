131ca846cdab0376170d8e09a9f9a261117dfcad
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-02-24 18:38:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;        
+        int answer = 45;
         
-        for(int i : numbers) {
-            answer -= i;
+        
+        for(int i=0; i<numbers.length; i++) {
+            answer -= numbers[i];    
         }
-          
+        
         return answer;
     }
 }
