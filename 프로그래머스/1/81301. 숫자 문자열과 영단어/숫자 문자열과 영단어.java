6dff3491fa83036d57862f709b6845cd098a8ccc
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-04-17 16:51:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,10 +1,10 @@
-//첫번째 풀이 다시 풀어보기
+//두번째 풀이 
 
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        
         Map<String, String> map = new HashMap<String, String>();
+        
         map.put("zero", "0");
         map.put("one", "1");
         map.put("two", "2");
@@ -16,13 +16,14 @@ class Solution {
         map.put("eight", "8");
         map.put("nine", "9");
         
-    
+        
+        
         for(String key : map.keySet()) {
             s = s.replace(key, map.get(key));
         }
-        
         int answer = Integer.parseInt(s);
         
         return answer;
+        
     }
 }
