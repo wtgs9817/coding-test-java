36bccdc947dbca551da3d3f17a2f0b1d4ba67d9e
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-04-19 14:54:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.12 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,22 @@
-//다시 풀어보기
+//두번째 풀이
 class Solution {
     public int solution(int a, int b, int n) {
+        
         int result = 0;
-
-        while (n >= a) {
-            int exchange = (n / a) * b; // 교환해서 받을 수 있는 콜라 수
+        
+        while(true) {
+           int exchange = b * (n/a);
             result += exchange;
-            n = (n % a) + exchange; // 남은 병 + 새로 마신 콜라의 병
+            
+           n = (n%a) + exchange;
+            
+            if(n < a) {
+                break;
+            }
         }
-
+        
         return result;
+        
     }
 }    
     
