8a60724c561a6f4209632d220a354e0f05f46e3d
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-05-04 17:14:27 +0900

    [level 1] Title: 실패율, Time: 217.26 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,54 +1,52 @@
-//세번째 풀이
+//네번째 풀이
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-    
-        Map<Integer, Double> map = new HashMap<Integer, Double>();
-        
-        
         int totalch = stages.length;
+        Map<Integer, Double> map = new HashMap<>();
+        
         for(int i=1; i<=N; i++) {
-            int ch = 0;    
+            int ch = 0;
             for(int k=0; k<stages.length; k++) {
-                if(stages[k] == i) {
+                if(i == stages[k]) {
                     ch++;
+                }   
+            }
+            
+            if(ch == 0) {
+                    map.put(i , 0.0);
                 }
-                
-            }
-            
-            if(totalch != 0) {
-                map.put(i, (double) ch /totalch );
-                totalch -= ch;    
-            }
-            
-            else {
-                map.put(i, 0.0);    
-            }
+                else {
+                    map.put(i, (double) ch / totalch);
+                    totalch -= ch;    
+                }             
         }
         
         List<Integer> list = new ArrayList<Integer>(map.keySet());
         
-        //정렬 여전히 바로바로 생각이 안남 좀 연습이 필요함
         list.sort((a1, a2) -> {
            int result = Double.compare(map.get(a2), map.get(a1));
             
-            if(result == 0) {
-                return Integer.compare(a1,a2);
+            if(result == 0 ) {
+                return Integer.compare(a1, a2);
             }
             
             else {
                 return result;
             }
+            
         });
         
-        int[] arr = new int[list.size()];
-        int cnt = 0;
-        for(int a : list) {
-            arr[cnt] = a;
-            cnt++;
-        }
+        int[] answer = new int[list.size()];
+        int count = 0;
         
-        return arr;
+        for(int num : list) {
+            answer[count] = num;
+            count++; 
+        }
+      
+        return answer;
+                
     }   
         
 }
