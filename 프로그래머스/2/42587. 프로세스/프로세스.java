0e13190f9fdf072f2b4fde2e22cc99d19686c522
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-06-02 09:35:15 +0900

    [level 2] Title: 프로세스, Time: 3.08 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,12 +1,67 @@
 import java.util.*;
 
 //첫번째 다시 풀어봐야 할 듯
+//두번째 풀이 - 해답을 안보고 맞췄지만 문제 좀 똑바로 읽어야 할 듯. 뻘짓거리로 시간 낭비함.
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         Queue<int[]> que = new LinkedList<>();
         
         
+        for(int i=0; i<priorities.length; i++) {
+            int[] arr = new int[2];
+            arr[0] = i;
+            arr[1] = priorities[i];
+            que.add(arr);
+        }
+        int count = 0;
+        while(!que.isEmpty()) {
+            int[] arr1 = que.poll();
+            boolean flag = false;
+            for(int[] arr2 : que)  {
+                
+                if(arr1[1] < arr2[1]) {
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if(flag == true) {
+                que.add(arr1);
+            }
+            
+            else {
+                count++;
+                if(arr1[0] == location) {
+                    answer= count;
+                } 
+            }
+            
+        }
+       
+        
+        
+       
+        
+       
+        
+        
+        
+        
+                
+       
+    
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        /*
         for(int i=0; i<priorities.length; i++) {
             int[] idx = new int[2];
             idx[0] = i;
@@ -35,7 +90,7 @@ class Solution {
             }
             
         }    
-            
+          */  
         return answer;
     }
 }
