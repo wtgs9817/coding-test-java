cd008ccaf3638ffa34499e0b48448b696077e773
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-08-14 15:00:10 +0900

    [level 1] Title: 모의고사, Time: 1.23 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,47 @@
+// 첫번째 풀이
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[][] s = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        int[] score = new int[s.length];
+        
+        for(int i=0; i<s.length; i++) {
+            int count = 0;
+            for(int k =0; k<answers.length; k++) {
+                if(answers[k] == s[i][k % s[i].length]) {
+                    count++;
+                }
+            }
+            score[i] = count;
+        }
+        
+        int maxCount = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        for(int i=0; i<score.length; i++) {
+            if(score[i] == maxCount) {
+                list.add(i+1);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        
+        return answer;
+        
+        
+    }
+    
+   
+    
+    
+    
+    
+    
+}
