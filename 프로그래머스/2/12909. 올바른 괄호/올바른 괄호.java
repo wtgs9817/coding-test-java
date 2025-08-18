c40f5ae7a859ee0c88a7b1b446e2fdc268af14e5
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-08-19 01:50:11 +0900

    [level 2] Title: 올바른 괄호, Time: 20.05 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,9 +1,76 @@
 //첫번째 큐로 풀이
 //두번째 스택으로 풀이
 //세번째 풀이도 스택
+//네번째 풀이
+//5번째 풀이
 import java.util.*;
 class Solution {
     boolean solution(String s) {
+    Stack<Character> stk = new Stack<>();
+    
+    
+        
+    for(char a : s.toCharArray()) {
+        if(a == '(') {
+            stk.push(a);
+        }
+        
+        else {
+            if(stk.isEmpty()) {
+                return false;
+            }
+            stk.pop();
+        }
+    }
+        
+    return stk.isEmpty();
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        
+        Stack<Character> stk = new Stack<>();
+        boolean answer = false;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stk.push(s.charAt(i));
+            }
+            else {
+                if(stk.isEmpty()) {
+                    answer = false;
+                    break;
+                }
+                stk.pop();
+                answer = true;
+            }            
+        }
+        
+        if(!stk.isEmpty()) {
+            answer = false;
+        }
+        */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         boolean answer = true;
         //Queue<Character> que = new LinkedList<>();
         Stack<Character> stk = new Stack<>();
@@ -36,6 +103,7 @@ class Solution {
             answer = false;
         }
         
+        */
         
         
         
@@ -97,11 +165,12 @@ class Solution {
         
         
         
-          */
+          
         
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("Hello Java");
 
         return answer;
+        */
     }
 }
