dd096e955e70458960ba6a4778ec156edc6ff241
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-08-22 02:36:40 +0900

    [level 2] Title: 피로도, Time: 3.30 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,7 +1,60 @@
 //첫번째 풀이 재귀 조금 헷갈림. 하기 전에 좀 더 보고 다시 풀어보면 좋을듯
 //두번째 풀이
 //세번째 풀이
+// 네번째 풀이
 class Solution {
+    static boolean[] visited;
+    static int cnt = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k,dungeons, cnt);
+        
+        return cnt;
+    
+    }
+    
+    public static void dfs(int k, int[][] dungeons, int count) {
+        
+        cnt = Math.max(cnt, count);
+        
+        for(int i=0; i<dungeons.length; i++) {
+            int cost = dungeons[i][1];
+            int required = dungeons[i][0];
+            
+            if(!visited[i] && k >= required) {
+                visited[i] = true;
+                dfs(k-cost, dungeons, count+1);
+                visited[i] = false;
+            }  
+        }
+        
+        
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
     int maxCount = 0;
     boolean[] visited;
     
@@ -33,7 +86,7 @@ class Solution {
     
     
     
-    
+    */
     
     
     
