d3cc39c89b6d183e5cb78ab14b12bb41403dae25
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-08-22 02:06:48 +0900

    [level 2] Title: 소수 찾기, Time: 27.14 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,8 +1,76 @@
 import java.util.*;
 
 //첫번째 풀이 많이 연습해봐야 할듯 많이 부족함.
-
+//두번째 풀이  
+// dfs(num + numbers.charAt(i), numbers); 이 부분 주의 num 에 값을 저장하고 재귀로 넘기면 재귀 끝나고 돌아왔을 때 
+// 값이 원복이 안됨.
 class Solution {
+    static boolean[] visited;
+    static Set<Integer> set = new HashSet<>();
+    
+    public int solution(String numbers) {
+        visited = new boolean[numbers.length()];
+        int count = 0;
+        
+        dfs("", numbers);
+        
+        for(int n : set) {
+            if(check(n)) {
+                count++;
+            }
+        }
+         
+        return count;
+    }
+    
+    
+    public static void dfs(String num, String numbers) {
+        
+        if(!num.isEmpty()) {
+            int n = Integer.parseInt(num);
+            set.add(n);
+        }
+       
+        
+        for(int i=0; i<numbers.length(); i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                
+                dfs(num + numbers.charAt(i), numbers);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public static boolean check(int num) {
+        if(num < 2) {
+            return false;
+        }
+        else {
+            for(int i=2; i<=Math.sqrt(num); i++) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    
     static boolean[] visited;
     static Set<Integer> set = new HashSet<>();
     
@@ -56,7 +124,7 @@ class Solution {
     }
     
     
-    
+    */
     
     
    
