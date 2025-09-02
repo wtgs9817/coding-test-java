8de0bcca64d1110cc2e364a748671fbbf71005de
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-09-02 19:16:39 +0900

    [level 2] Title: 타겟 넘버, Time: 4.29 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,41 @@
 //첫번째 풀이 다시 풀어보기
 //두번째 풀이
+//세번째 풀이 매개변수 실수만 안하면 될듯. 거의 완벽
 class Solution {
+    static int count = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);    
+        return count;
+    }
+    
+    public static void dfs(int depth, int num ,int[] numbers, int target) {
+        if(depth == numbers.length) {
+            if(num == target) {
+                count++;
+            }
+            
+            return;
+        }
+        
+        
+        dfs(depth+1, num+numbers[depth], numbers, target);
+        dfs(depth+1, num-numbers[depth], numbers, target);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
     static int count = 0;
     public int solution(int[] numbers, int target) {
         
@@ -23,7 +58,7 @@ class Solution {
                 
     }
     
-    
+    */
     
     
     
