c56a9a12df0ae6f38574c7b3aee8625cd61a13a1
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-09-06 01:12:43 +0900

    [level 3] Title: 단어 변환, Time: 0.92 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,53 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        
-        if(!Arrays.asList(words).contains(target)) {
-            return 0;
-        }
-        
-        
-        Queue<Word> que = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
-        
-        que.add(new Word(begin, 0));
-        
-        while(!que.isEmpty()) {
-            Word current = que.poll();
-            
-            if(current.str.equals(target)) {
-                return current.step;
-            }
-            
-            for(int i=0; i<words.length; i++) {
-                if(!visited[i] && check(current.str, words[i])==1) {
-                    que.add(new Word(words[i], current.step+1));
-                    visited[i] = true;   
-                }
-            }          
-        }
-            
-        return 0;
-    }
-    
-    
-    public int check(String str, String str2) {
-        int count = 0;
-        for(int i=0; i<str2.length(); i++) {
-            if(str.charAt(i) != str2.charAt(i)) {
-                count++;
-            }
-        } 
-        return count;
-    }
-    
-    public class Word {
-        String str;
-        int step;
-        public Word(String str, int step) {
-            this.str = str;
-            this.step = step;
-        }
+        int answer = 0;
+        return answer;
     }
 }
