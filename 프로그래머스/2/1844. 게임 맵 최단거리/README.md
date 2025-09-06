cc661cef75c69148167ea12f6798e7d378e85c17
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-09-06 16:02:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.28 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?utm_source=chatgpt.com) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 7.48 ms
+메모리: 56.3 MB, 시간: 11.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 02일 01:44:31
+2025년 09월 06일 16:02:05
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,66 @@
 import java.util.*;
 //첫번째 풀이
+//두번째 풀이  한번은 더 풀어보자
 class Solution {
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        int y = maps.length;
+        int x = maps[0].length;
+        
+        Queue<int[]> que = new ArrayDeque<>();
+        int[][] visited = new int[y][x];
+        
+        
+        
+        int[] nx = {0,0,-1,1}; //상하좌우
+        int[] ny = {-1,1,0,0};
+        
+        que.offer(new int[]{0,0});
+        visited[0][0] = 1;
+        
+        
+        
+        while(!que.isEmpty()) {
+            int[] loc = que.poll();
+            
+            int lx = loc[1];
+            int ly = loc[0];
+            
+            if(lx == x-1 && ly == y-1 ) {
+                answer = visited[ly][lx];
+            }
+            
+            for(int i=0; i<4; i++) {
+                int xx = lx+nx[i];
+                int yy = ly+ny[i];
+                
+                if(xx < 0 || xx >= x || yy < 0 || yy >= y) {
+                    continue;
+                }
+                
+                if(maps[yy][xx] == 1 && visited[yy][xx] == 0) {
+                    que.offer(new int[]{yy,xx});
+                    visited[yy][xx] = visited[ly][lx] + 1;
+                }                
+            }  
+        }
+        
+        if(answer == 0) {
+            answer = -1;
+        }
+        
+        return answer;
+    }
     
+    
+    
+    
+    
+    
+    
+    
+    /*
     
     public int solution(int[][] maps) {
         int answer = bfs(maps);
@@ -45,4 +104,6 @@ class Solution {
         return visited[n-1][m-1] == 0 ? -1 : visited[n-1][m-1];
 
     }
+    
+    */
 }
