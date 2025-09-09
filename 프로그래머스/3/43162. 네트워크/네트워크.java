4044f27dd7a38517c51fc9a51f0682f481663f27
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-09-10 02:34:42 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,76 @@
 //첫번째 풀이 꼭 다시 풀어보기
-//두번째 풀이  
+//두번째 풀이
+//세번째 풀이
 class Solution {
+    int n;
+    int[][] computers;
+    boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        int count = 0;
+        
+        this.n = n;
+        this.computers = computers;
+        visited = new boolean[n];
+        
+        for(int i=0; i<computers.length; i++) {
+            if(!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(int num) {
+        visited[num] = true;
+        
+        for(int i=0; i<computers[num].length; i++) {
+            if(!visited[i] && computers[num][i] == 1) {
+                dfs(i);
+            }        
+        }        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    
+    
     int n;
     int[][] computers;
     boolean[] visited;
@@ -34,7 +104,7 @@ class Solution {
     
     
     
-    
+    */
     
     
     
