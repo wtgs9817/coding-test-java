8dd8193392d923c9dfccc5a1b264ebcb26f170b0
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-10-25 00:59:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 66.42 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,7 +1,75 @@
 import java.util.*;
+
+// 두번째 풀이 
 class Solution {
     public int solution(String s) {
+        int len = s.length();
+        int answer = 0;
         
+        for(int i=0; i<len; i++) {
+            Stack<Character> stk = new Stack<>();
+            String str = s.substring(i) + s.substring(0,i);
+            boolean flag = true;
+            int count = 0;
+            
+            
+            for(int k=0; k<len; k++) {
+                if(str.charAt(k) == '(' || str.charAt(k) == '{' || str.charAt(k) == '[') {
+                    stk.push(str.charAt(k));
+                    count++;
+                }
+                
+                else {
+                    if(count > 0) {
+                        char ch = stk.pop();
+                        if(ch == '(') {
+                            if(str.charAt(k) == ')') {
+                                count--;    
+                            }
+                            else {
+                                flag = false;
+                            }
+                            
+                        }
+                        
+                        if(ch == '[') {
+                            if(str.charAt(k) == ']') {
+                                count--;   
+                            }
+                            else {
+                                flag = false;
+                            }
+                        }
+                        
+                        if(ch == '{') {
+                            if(str.charAt(k) == '}') {
+                                count--;
+                            }
+                            else {
+                                flag = false;
+                            }
+                        }
+                        
+                        
+                    }
+                    else {
+                        flag = false;
+                    }
+                }
+                if(!flag) break;
+            }
+            
+            if(flag && stk.isEmpty()) answer++;
+        }
+        
+        
+        
+        
+        return answer;
+        
+        
+        
+        /*
         int answer = 0;
         for(int i=0; i<s.length(); i++) {
             boolean flag = true;
@@ -43,5 +111,7 @@ class Solution {
         }
         
         return answer;
+        
+        */
         }
     }
