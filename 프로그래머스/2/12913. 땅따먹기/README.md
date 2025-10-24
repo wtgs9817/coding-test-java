5d77f1adc0f2884573f0fe69ae0fc097f3161b8f
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-10-24 20:02:33 +0900

    [level 2] Title: 땅따먹기, Time: 21.48 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?utm_source=chatgpt.com) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 23.28 ms
+메모리: 87.5 MB, 시간: 21.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 23일 11:22:27
+2025년 10월 24일 20:02:29
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,6 +1,38 @@
+//두번째 풀이
+
 class Solution {
+    //n행 4열  
+    
     
     int solution(int[][] land) {
+        
+        for(int i=1; i<land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][2], land[i-1][3]), land[i-1][0]);
+            land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][3]), land[i-1][0]);
+            land[i][3] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][1]);
+        }
+        
+        int n = land.length-1;
+        
+        int answer = Math.max(Math.max(land[n][0], land[n][1]), Math.max(land[n][2], land[n][3]));
+        
+        return answer;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         int[] prev = new int[4];
         
         for(int i=1; i<land.length; i++) {
@@ -22,5 +54,8 @@ class Solution {
         System.out.println("Hello Java");
 
         return answer;
+        
+        
+        */
     }
 }
