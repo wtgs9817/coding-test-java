3e8837b79d9522e0fd72e57019422c35b575e36c
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-11-26 03:19:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 16544 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -5,62 +5,46 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	/*
-	N개의 랜선 만들어야 됨.
-	K개 랜선은 길이 제각각 
-	랜선을 모두 N개의 같은 길이로 만들려고 함.
-	K에서 잘라서 만드려고 하는데 만들고 남은 길이는 버림
-	이런 과정을 거쳐서 만들 수 있는 최대 N개의 랜선을 구하셈
-	
-	*/
-	
-	public static int K; //갖고 있는 랜선 수
-	public static int N; //필요 랜선
-	public static long[] arr;
-	
-	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		K = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		
-		arr = new long[K];
-		long max = 0;
-		
-		for(int i=0; i<K; i++) {
+		long answer = 0;
+		int[] arr = new int[N];
+		long high = 0;
+		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			max = Math.max(max, arr[i]);
+			if(high < arr[i]) high = arr[i];
 		}
 		
-		long answer = check(max);
-		System.out.println(answer);
 		
-	}
-	
-	public static long check(long max) {
 		long low = 1;
-		long high = max;
-		long answer = 0;
 		
 		while(low <= high) {
 			long mid = (low + high)/2;
-			long cnt = 0;
+			int count = 0;
 			
-			for(int i=0; i<K; i++) {
-				cnt = cnt + (arr[i]/mid);
+			
+			for(int a : arr) {
+				count += (a/mid);
 			}
 			
-			if(cnt < N) {
-				high = mid - 1;
+			
+			if(count < M) {
+				high = mid -1;	
 			}
-			else {
+			
+			else if(count >= M) {
 				answer = mid;
-				low = mid + 1;
-			}	
+				low = mid +1;
+				
+			}
 		}
-		return answer;
+		
+		System.out.println(answer);
 	}
+
 }
