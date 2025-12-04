82103b75381ea7f494f595676219157da0692ae6
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-12-04 17:15:32 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,39 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.*;
 
 public class Main {
+
 	
+	//다시 풀어봄
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer> que = new ArrayDeque<>();
-		Stack<Integer> stk = new Stack<>();
+		ArrayDeque<Integer> que = new ArrayDeque<>();
+		Stack<Integer> stk = new Stack<>();	
 		
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		
 		for(int i=0; i<N; i++) {
 			que.offer(Integer.parseInt(st.nextToken()));
 		}
-		int count = 1;
+		int order = 1;
 		
 		while(!que.isEmpty() || !stk.isEmpty()) {
 			
-			if(!que.isEmpty() && que.peek() == count) {
+			if(!que.isEmpty() && order == que.peek()) {
 				que.poll();
-				count++;
-			}
-			else if(!stk.isEmpty() && stk.peek() == count) {
-				stk.pop();
-				count++;
+				order++;
 			}
 			
-			else if(!que.isEmpty()) {
-				stk.push(que.poll());
+			else if(!stk.isEmpty() && stk.peek() == order) {
+				stk.pop();
+				order++;
 			}
+			
+			else if(!que.isEmpty()) stk.push(que.poll());
 			
 			else {
 				System.out.println("Sad");
@@ -41,8 +40,7 @@ public class Main {
 			}
 		}
 		
-		
-		System.out.println("Nice");
-		
+		System.out.println("Nice");	
 	}
+
 }
