2e0b8ed8c64b8eabf7e26e2130bb5893f1db635d
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2025-12-24 21:17:33 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -4,43 +4,38 @@ import java.io.InputStreamReader;
 
 public class Main {
 	
+	
+	//두번째 풀이
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<T; i++) {
+			int n = Integer.parseInt(br.readLine());
 			
-			int num = Integer.parseInt(br.readLine());
-			
-			if(num == 0 ) {
-				System.out.println(1 + " " + 0);
-				continue;
+			if(n == 0) System.out.println(1 + " " + 0 );
+			else if(n == 1) System.out.println(0 + " " + 1);
+			else {
+				int[] arr0 = new int[n+1];
+				int[] arr1 = new int[n+1];
+				
+				arr0[0] = 1;
+				arr0[1] = 0;
+				
+				arr1[0] = 0;
+				arr1[1] = 1;
+				
+				
+				for(int k=2; k<=n; k++) {
+					arr0[k] = arr0[k-1] + arr0[k-2];
+					arr1[k] = arr1[k-1] + arr1[k-2];
+				}
+				
+				System.out.println(arr0[n] + " " + arr1[n]);
 			}
 			
-			else if(num == 1) {
-				System.out.println(0 + " " + 1);
-				continue;
-			}
-			
-			
-			int[] zero = new int[num+1];
-			int[] one = new int[num+1];
-			
-			zero[0] = 1;
-			zero[1] = 0;
-			
-			one[0] = 0;
-			one[1] = 1;
-			
-			
-			for(int k=2; k<=num; k++) {
-				zero[k] = zero[k-1] + zero[k-2];
-				one[k] = one[k-1] + one[k-2];
-			}
-			
-			System.out.println(zero[num] + " " + one[num]);
-		}	
+		}
+		
 	}
+
 }
