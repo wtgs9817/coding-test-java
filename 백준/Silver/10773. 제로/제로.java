a3d7448515236816121c350234b6e2e027e9ac3f
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-01-08 16:48:09 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 22944 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -6,24 +6,25 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args)throws IOException {
-	
-		Stack<Integer> stk = new Stack<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int sum = 0;
 		
-		for(int i=0; i<N; i++) {
+		int K = Integer.parseInt(br.readLine());
+		int[] arr = new int[K];
+		
+		int idx = 0;
+		int sum = 0;
+		for(int i=0; i<K; i++) {
 			int n = Integer.parseInt(br.readLine());
 			
-			if(n == 0) {
-				sum -= stk.pop();
+			if(n != 0) {
+				arr[idx++] = n;
+				sum+= n;
 			}
 			else {
-				stk.push(n);
-				sum += n;
+				sum-=arr[--idx];
+				arr[idx] =0;
 			}
 		}
-		
 		System.out.println(sum);
 	}
 }
