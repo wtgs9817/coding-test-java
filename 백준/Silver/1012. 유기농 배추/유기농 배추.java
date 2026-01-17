68f5c14f2f90a04dd9928fbc57f239a802b19e62
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-01-17 11:40:50 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -3,70 +3,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
-	
-	static int[][] arr;
-	
 	//상 하 좌 우
-	static int[] X = {0, 0, -1, 1};
-	static int[] Y = {-1, 1, 0, 0};
+	static int[] X = {0,0,-1,1};
+	static int[] Y = {-1,1,0,0};
 	
-	static int x,y,k;
 	
+	static int N,M,K;
+	static int[][] map;
+	static Queue<int[]> que;
+	static boolean[][] visited;
 	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int t = Integer.parseInt(br.readLine());
 		
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<T; i++) {
+		for(int i=0; i<t; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			k = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken()); // 가로
+			N = Integer.parseInt(st.nextToken()); // 세로
+			K = Integer.parseInt(st.nextToken());
 			
-			int count = 0;
-			Queue<int[]> que = new ArrayDeque<>();
-			arr = new int[y][x];
-			boolean[][] visited = new boolean[y][x];
-			
-			for(int q=0; q<k; q++) {
-				st = new StringTokenizer(br.readLine());
+			map = new int[N][M];
+			que = new ArrayDeque<>();
+			visited = new boolean[N][M];
 	
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
+			int count = 0;
+			for(int z=0; z<K; z++) {
+				st = new StringTokenizer(br.readLine());
 				
-				que.offer(new int[] {a,b});
-				arr[b][a] = 1;
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				map[y][x] = 1;
+				que.offer(new int[] {y,x});
 			}
-			for(int[] loc : que) {
-				if(!visited[loc[1]][loc[0]]) {
-					search(loc, visited);
+			
+			for(int[] arr : que) {
+				int lx = arr[1];
+				int ly = arr[0];
+				
+				if(!visited[ly][lx]) {
+					search(ly, lx);
 					count++;
 				}
 			}
-			System.out.println(count);			
+		
+			System.out.println(count);
 		}
 	}
 	
-	public static void search(int[] loc, boolean[][] visited) {
-		int lx = loc[0];
-		int ly = loc[1];
-		visited[ly][lx] = true;
+	public static void search(int y, int x) {
+		visited[y][x] = true;
 		
 		for(int i=0; i<4; i++) {
-			int dx = lx + X[i];
-			int dy = ly + Y[i];
+			int dx = x + X[i];
+			int dy = y + Y[i];
 			
-			if(dx < 0 || dx >= x || dy < 0 || dy >= y) continue;
-			
-			if(arr[dy][dx] == 1) {
-				if(!visited[dy][dx]) {
-					search(new int[] {dx,dy}, visited);
-				}
+			if(dx < 0 || dx >= M || dy < 0 || dy >=N) continue;
+			if(map[dy][dx] == 1 && !visited[dy][dx]) {
+				search(dy,dx);
 			}
-		}	
+		}
+
 	}
+
 }
