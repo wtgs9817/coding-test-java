3682d014072340b2027d887a27a57e3aeab7d1d4
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-01-19 18:41:04 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 43828 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -3,34 +3,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 
+회의의 시작시간과 끝나는 시간이 같을 수도 있다.
+*/
+
+//복습
 public class Main {
+	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
+		int[][] cf = new int[N][2];
 		
-		int[][] meetings = new int[N][2];
+		StringTokenizer st;
 		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			meetings[i][0] = Integer.parseInt(st.nextToken()); // 회의 시작시간
-			meetings[i][1] = Integer.parseInt(st.nextToken()); // 회의 종료시간
+			st = new StringTokenizer(br.readLine());
+			
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			
+			cf[i][0] = s;
+			cf[i][1] = e;
 		}
 		
-		//끝나는 시간대로 정렬  같으면 작은 순으로
-		Arrays.sort(meetings, (a,b) -> {
+		Arrays.sort(cf, (a,b) -> {
 			if(a[1] == b[1]) return a[0] - b[0];
 			return a[1] - b[1];
 		});
 		
+		int start = 0;
 		int count = 0;
-		int end = 0;
-		
-		for(int[] meeting : meetings) {
-			if(end <= meeting[0]) {  //이전 종료 시간 <= 시작 시간
-				end = meeting[1];
+		for(int[] arr : cf) {
+			int s = arr[0];
+			int e = arr[1];
+			
+			if(start <= s) {
+				start = e;
 				count++;
 			}
 		}
 		System.out.println(count);
 	}
+
 }
