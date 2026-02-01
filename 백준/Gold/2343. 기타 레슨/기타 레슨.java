d2d35efd3a94e587340d3423199d9e3b737a5524
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-02-01 20:26:18 +0900

    [Gold V] Title: 기타 레슨, Time: 260 ms, Memory: 23732 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -1,57 +1,53 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
+//복습
 public class Main {
+	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int result = 0;
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken()); //강의 수
+		int M = Integer.parseInt(st.nextToken()); //블루레이 수
 		
 		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
 		
+		st = new StringTokenizer(br.readLine());
 		int min = 0;
 		int max = 0;
 		
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			max += arr[i];
-			if(arr[i] > min) min = arr[i];
+			if(min < arr[i]) min = arr[i];
+			max+=arr[i];
 		}
-		int num = max;
-		int result = 0;
 		
-		while(min <= max) {
+		while(min<=max) {
 			int mid = (min + max) / 2;
 			int count = 0;
 			int sum = 0;
+			
 			for(int i=0; i<N; i++) {
-				if(sum + arr[i] > mid) {
+				if(mid < sum + arr[i]) {
 					count++;
 					sum = 0;
 				}
-				sum += arr[i];
+				sum+=arr[i];
 			}
-			if(sum > 0 ) count++;
+			if(sum != 0) count++;
 			
 			if(count <= M) {
 				result = mid;
 				max = mid - 1;
 			}
-			else {
-				min = mid + 1;
-			}
-			
+			else min = mid + 1;
 		}
 		
 		System.out.println(result);
-	
 	}
-
 }
