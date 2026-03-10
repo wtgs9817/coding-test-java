40a44e4408dfd8edcd1959d7f58b08f86aa59b4e
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-03-10 22:08:25 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,57 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		//상 하 좌 우
+		int[] X = {0,0,-1,1};
+		int[] Y = {-1,1,0,0};
+		ArrayDeque<int[]> que = new ArrayDeque<>();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
+		int[][] map = new int[N+1][M+1];
 		
-		//상하좌우
-		int[] X = {0,0,-1,1};
-		int[] Y = {-1,1,0,0};
-				
-		int[][] arr = new int[N][M];
-		
-		for(int i=0; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			String num = br.readLine();
-			for(int k=0; k<M; k++) {
-				arr[i][k] = num.charAt(k) - '0';
+			for(int k=1; k<=M; k++) {
+				char n = num.charAt(k-1);
+				if(n == '1') {
+					map[i][k] = Character.getNumericValue(n);
+				}
 			}
 		}
 		
-		Queue<int[]> que = new ArrayDeque<>();
-		que.offer(new int[] {0,0});
 		
-	
+		que.offer(new int[] {1,1});
+		boolean flag = false;
 		while(!que.isEmpty()) {
-			
-			if(arr[N-1][M-1] > 1) break;
-			
+			if(flag) break;
 			int[] loc = que.poll();
-			int x = loc[1];
-			int y = loc[0];
 			
+			int x = loc[1]; 
+			int y = loc[0];
 			
 			for(int i=0; i<4; i++) {
 				int dx = x + X[i];
 				int dy = y + Y[i];
 				
-				if(dx < 0 || dx >= M || dy < 0 || dy >= N) continue;
+				if(dx < 1 || dx >M || dy < 1 || dy > N) continue;
+				if(map[dy][dx] != 1) continue;
+				if(dx == 1 && dy ==1 ) continue;
 				
-				if(arr[dy][dx] == 1) {
-					arr[dy][dx] += arr[y][x];
-					que.offer(new int[] {dy,dx});
+				map[dy][dx] = map[y][x] + 1;
+				if(dy == N && dx == M) {
+					flag = true;
+					break;
 				}
+				que.offer(new int[] {dy, dx});
 			}
 		}
 		
-		System.out.println(arr[N-1][M-1]);
-		
+		System.out.println(map[N][M]);
 	}
 }
