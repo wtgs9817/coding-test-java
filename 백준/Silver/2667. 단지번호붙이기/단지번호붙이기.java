f39d5bab461a6f62e67c90e3b0ad447567548970
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-03-22 23:50:24 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,80 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
+//복습
 public class Main {
-	
-	public static int[][] arr;
-	public static boolean[][] visited;
-	public static int N;
-	
-	public static int cnt = 0;
-	
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int cnt = 0;
 	//상 하 좌 우
-	public static int[] X = {0,0,-1,1};
-	public static int[] Y = {-1,1,0,0};
+	static int[] X = {0,0,-1,1};
+	static int[] Y = {-1,1,0,0};
 	
-	
-	
-	
-	public static void main(String[] args)throws IOException { 
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N][N];
+		map = new int[N][N];
 		visited = new boolean[N][N];
-		List<Integer> list = new ArrayList<>();
-		
+		PriorityQueue<Integer> que = new PriorityQueue<>();
 		
 		for(int i=0; i<N; i++) {
 			String str = br.readLine();
 			for(int k=0; k<N; k++) {
-				arr[i][k] = Integer.parseInt(String.valueOf(str.charAt(k)));
+				int n = str.charAt(k)-'0';
+				map[i][k] = n;
 			}
 		}
 		
 		for(int i=0; i<N; i++) {
 			
 			for(int k=0; k<N; k++) {
-				if(!visited[i][k] && arr[i][k] == 1) {
-					searchAPT(i,k);
-					list.add(cnt);
-					cnt = 0; //단지하나를 다 탐색한 뒤 초기화
+				if(map[i][k] == 1 && !visited[i][k]) {
+					cnt = 1;
+					search(i, k);
+					que.offer(cnt);
 				}
 			}
+			cnt = 0;
 		}
-
+		StringBuilder sb = new StringBuilder();
+		sb.append(que.size()).append("\n");
 		
-		Collections.sort(list);
-		System.out.println(list.size());
-		for(int a : list) System.out.println(a);
+		//우선순위 큐는 그냥 반복문으로 돌리면 오름차순 보장X
+		//poll()로 하나씩 꺼낼 때만 최솟값 순서로 보장 O
+		while(!que.isEmpty()) {
+			sb.append(que.poll()).append("\n");
+		}
 		
-		
+		System.out.println(sb);
 	}
 	
-	public static void searchAPT(int i, int k) {
-		visited[i][k] = true;
-		cnt++;
+	static void search(int y, int x) {
+		visited[y][x] = true;
 		
-		int x = k;
-		int y = i;
-		
-		for(int w=0; w<4; w++) {
-			int cx = x + X[w];
-			int cy = y + Y[w];
+		for(int i=0; i<4; i++) {
+			int dx = x + X[i];
+			int dy = y + Y[i];
 			
-			if(cx < 0 || cx >=N || cy < 0 || cy >=N) continue;
+			if(dx < 0 || dx >=N || dy < 0 || dy >=N) continue;
+			if(map[dy][dx] != 1 || visited[dy][dx]) continue;
 			
-			
-			if(!visited[cy][cx] && arr[cy][cx] == 1) {
-				searchAPT(cy, cx);
-			}
-		}
+			cnt++;
+			search(dy,dx);
+		}	
 	}
-	
 }
