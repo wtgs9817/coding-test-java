d329ae8dbbf12153c3140bcef19528859febc6fd
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-04-20 23:11:09 +0900

    [Gold IV] Title: 연구소, Time: 924 ms, Memory: 302024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,21 +1,23 @@
 import java.util.*;
 import java.io.*;
 
+
+//복습
 public class Main {
+	static int N,M;
+	static int[][] map;
+	static int result = 0;
 	
 	//상 하 좌 우
 	static int[] X = {0,0,-1,1};
 	static int[] Y = {-1,1,0,0};
 	
-	static int[][] map;
-	static int N,M;
-
-	static int answer = 0;
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
@@ -28,16 +30,14 @@ public class Main {
 			}
 		}
 		
-		search(0);
-		System.out.println(answer);
-		
-		
-		
+		dfs(0);
+		System.out.println(result);
 	}
 	
-	static void search(int count) {
-		if(count == 3) {
-			answer = Math.max(answer, bfs());
+	static void dfs(int depth) {
+		
+		if(depth == 3) {
+			result = Math.max(result, safeArea());
 			return;
 		}
 		
@@ -46,51 +46,49 @@ public class Main {
 			for(int k=1; k<=M; k++) {
 				if(map[i][k] == 0) {
 					map[i][k] = 1;
-					search(count+1);
+					dfs(depth+1);
 					map[i][k] = 0;
-				}	
+				}
 			}
-		}		
+		}
 	}
 	
-	static int bfs() {
-		int count = 0;
+	static int safeArea() {
+		int cnt = 0;
 		ArrayDeque<int[]> que = new ArrayDeque<>();
 		int[][] temp = new int[N+1][M+1];
-		
+				
 		for(int i=1; i<=N; i++) {
 			
-			for(int k=1; k<=M;  k++) {
+			for(int k=1; k<=M; k++) {
 				temp[i][k] = map[i][k];
-				if(map[i][k] == 2) que.offer(new int[] {i,k});
+				if(temp[i][k] == 2) que.offer(new int[] {i,k});
 			}
 		}
 		
 		while(!que.isEmpty()) {
-			int[] loc = que.poll();
+			int[] arr = que.poll();
 			
-			int x = loc[1];
-			int y = loc[0];
+			int x = arr[1];
+			int y = arr[0];
 			
 			for(int i=0; i<4; i++) {
 				int dx = x + X[i];
 				int dy = y + Y[i];
 				
-				if(dx < 1 || dx > M || dy < 1 || dy > N) continue;
-				if(temp[dy][dx] == 0) {
+				if(dx < 1 || dx > M || dy < 1 || dy > N ) continue;
+				if(temp[dy][dx]==0) {
 					temp[dy][dx] = 2;
 					que.offer(new int[] {dy,dx});
 				}
-			}
+			}	
 		}
-		int cnt = 0;
+		
 		for(int i=1; i<=N; i++) {
 			for(int k=1; k<=M; k++) {
 				if(temp[i][k] == 0) cnt++;
 			}
 		}
-		
 		return cnt;
 	}
-	
 }
