6e3eb443ac3c2222f93f837c96e7e32e91dd4b50
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-04 22:57:48 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,146 +1,28 @@
-//첫번째 풀이 꼭 다시 풀어보기
-//두번째 풀이
-//세번째 풀이
 class Solution {
-    int n;
-    int[][] computers;
-    boolean[] visited;
-    
+    static int n2 = 0;
     public int solution(int n, int[][] computers) {
-        int count = 0;
-        
-        this.n = n;
-        this.computers = computers;
-        visited = new boolean[n];
-        
-        for(int i=0; i<computers.length; i++) {
-            if(!visited[i]) {
-                dfs(i);
-                count++;
-            }
-        }
-        return count;
-    }
-    
-    public void dfs(int num) {
-        visited[num] = true;
-        
-        for(int i=0; i<computers[num].length; i++) {
-            if(!visited[i] && computers[num][i] == 1) {
-                dfs(i);
-            }        
-        }        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    
-    
-    int n;
-    int[][] computers;
-    boolean[] visited;
-    
-    public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.computers = computers;
-        this.visited = new boolean[n];
-        
-        int count = 0;
+        boolean[] visited = new boolean[n];
+        int result = 0;
+        n2 = n;
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                dfs(i);
-                count++;
+                visited[i] = true;
+                dfs(i, visited, computers);
+                result++;
             }
         }
         
-        return count;
+        return result; 
     }
     
-    public void dfs(int num) {
-        visited[num] = true;
+    static void dfs(int num, boolean[] visited, int[][] com) {
         
-        for(int i=0; i<this.n; i++) {
-            if(this.computers[num][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
-    
-    
-    
-    
-    */
-    
-    
-    
-    
-    
-    /*
-    
-    int n;
-    boolean[] visited;
-    int[][] computers;
-    public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.visited = new boolean[this.n];
-        this.computers = computers;
-        
-        int count =0;
-        for(int i=0; i<n; i++) {
-            if(!visited[i]) {
-                dfs(i);
-                count++;    
+        for(int i=0; i<n2; i++) {
+            if(i != num && !visited[i] && com[num][i] == 1) {
+                visited[i] = true;
+                dfs(i, visited, com);
             }
         }
         
-        return count;
     }
-    
-    public void dfs(int num) {
-        
-        this.visited[num] = true;
-        for(int i=0; i<this.n; i++) {
-            if(computers[num][i] == 1 && !this.visited[i]) {
-                dfs(i);
-            }
-        }
-    }
-    
-    */
-    
 }
