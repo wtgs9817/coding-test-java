eace6d9b605244deb08ff30fdb531598160a140a
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-04 21:55:28 +0900

    [level 2] Title: 타겟 넘버, Time: 6.67 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,97 +1,30 @@
-//첫번째 풀이 다시 풀어보기
-//두번째 풀이
-//세번째 풀이 매개변수 실수만 안하면 될듯. 거의 완벽
 class Solution {
-    static int count = 0;
+    static int tar = 0;
+    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(0, 0, numbers, target);    
-        return count;
+        
+        tar = target;
+        int[] arr = new int[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++) {
+            arr[i] = numbers[i];
+        }
+        
+        dfs(0, arr, 0);
+        return answer;
     }
     
-    public static void dfs(int depth, int num ,int[] numbers, int target) {
-        if(depth == numbers.length) {
-            if(num == target) {
-                count++;
-            }
+    static void dfs(int depth, int[] arr, int result) {
+        
+        if(depth == arr.length) {
+            if(result == tar) answer++;
             
             return;
         }
         
-        
-        dfs(depth+1, num+numbers[depth], numbers, target);
-        dfs(depth+1, num-numbers[depth], numbers, target);
+        dfs(depth+1, arr, result + arr[depth]);
+        dfs(depth+1, arr, result - arr[depth]);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    static int count = 0;
-    public int solution(int[] numbers, int target) {
-        
-        dfs(0, 0, numbers, target);
-        return count;
-        
-    }
-    
-    
-    public static void dfs(int a, int depth, int[] numbers, int target) {
-        if(depth == numbers.length) {
-            if(a == target) {
-                count++;    
-            }
-            return;
-        }
-        
-        dfs(a + numbers[depth], depth+1, numbers, target);
-        dfs(a - numbers[depth], depth+1, numbers, target);
-                
-    }
-    
-    */
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    static int count = 0;
-    
-    public int solution(int[] numbers, int target) {
-        int len = numbers.length;
-        
-        dfs(0,0, numbers, target);
-        
-        return count;
-        
-    }
-    
-    public static void dfs(int depth, int sum, int[] numbers, int target) {
-        if(depth == numbers.length) {
-            if(sum == target) {
-                count++;
-            }
-            return;
-        }
-        
-        dfs(depth+1, sum + numbers[depth], numbers, target);
-        dfs(depth+1, sum - numbers[depth], numbers, target);
-        
-        
-    }
-    
-    */
+
 }
