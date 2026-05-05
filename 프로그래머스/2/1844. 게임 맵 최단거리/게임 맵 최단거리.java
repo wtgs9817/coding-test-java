0811e7a3f56a8279085c502a53785d81f7f74302
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-05 19:26:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.59 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,109 +1,39 @@
 import java.util.*;
-//첫번째 풀이
-//두번째 풀이  한번은 더 풀어보자
+
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
+        //상 하 좌 우
+        int[] X = {0,0,-1,1};
+        int[] Y = {-1,1,0,0};
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        int a = maps.length;
+        int b = maps[0].length;
         
-        int y = maps.length;
-        int x = maps[0].length;
+        int[][] result = new int[a][b];
         
-        Queue<int[]> que = new ArrayDeque<>();
-        int[][] visited = new int[y][x];
-        
-        
-        
-        int[] nx = {0,0,-1,1}; //상하좌우
-        int[] ny = {-1,1,0,0};
-        
+        result[0][0] = 1;
         que.offer(new int[]{0,0});
-        visited[0][0] = 1;
-        
-        
         
         while(!que.isEmpty()) {
-            int[] loc = que.poll();
+            int[] arr = que.poll();
             
-            int lx = loc[1];
-            int ly = loc[0];
-            
-            if(lx == x-1 && ly == y-1 ) {
-                answer = visited[ly][lx];
-            }
+            int x = arr[1];
+            int y = arr[0];
             
             for(int i=0; i<4; i++) {
-                int xx = lx+nx[i];
-                int yy = ly+ny[i];
+                int dx = X[i] + x;
+                int dy = Y[i] + y;
                 
-                if(xx < 0 || xx >= x || yy < 0 || yy >= y) {
-                    continue;
-                }
+                if(dx < 0 || dx >= b || dy < 0 || dy >= a) continue;
                 
-                if(maps[yy][xx] == 1 && visited[yy][xx] == 0) {
-                    que.offer(new int[]{yy,xx});
-                    visited[yy][xx] = visited[ly][lx] + 1;
-                }                
-            }  
-        }
-        
-        if(answer == 0) {
-            answer = -1;
-        }
-        
-        return answer;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    
-    public int solution(int[][] maps) {
-        int answer = bfs(maps);
-        return answer;
-    }
-    
-    
-    
-    public static int bfs(int[][] maps) {
-        int[] dx = {-1,1,0,0};  //y축 상하좌우
-        int[] dy = {0,0,-1,1}; 
-        
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        Queue<int[]> que = new ArrayDeque<>();
-        int[][] visited = new int[n][m];
-        que.offer(new int[]{0,0});
-        visited[0][0] = 1;
-        
-        while(!que.isEmpty()) {
-            int[] current = que.poll();
-            int x = current[0];
-            int y = current[1];
-            
-            for(int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                
-                if(maps[nx][ny] == 1 && visited[nx][ny] == 0) {
-                    que.offer(new int[]{nx, ny});
-                    visited[nx][ny] = visited[x][y] + 1;
+                if(maps[dy][dx] == 1 && result[dy][dx] == 0) {
+                    que.offer(new int[]{dy,dx});
+                    result[dy][dx] = result[y][x] + 1;
+                    
+                    if(dy == (a-1) && dx == (b-1)) return result[dy][dx];
                 }
             }
         }
-        
-        return visited[n-1][m-1] == 0 ? -1 : visited[n-1][m-1];
-
+        return result[a-1][b-1] == 0 ? -1 : result[a-1][b-1];
     }
-    
-    */
 }
