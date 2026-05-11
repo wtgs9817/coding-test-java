f0b94cdd99ba48c4824e97dd5d16c62373a44926
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-11 21:35:32 +0900

    [level 1] Title: 최소직사각형, Time: 0.90 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,57 +1,24 @@
-
-//3번째 풀이  
-//4번째 풀이
 class Solution {
-    
-        public int solution(int[][] sizes) {
-        int maxWidth = Math.max(sizes[0][0], sizes[0][1]);
-        int maxHeight = Math.min(sizes[0][0], sizes[0][1]);
-        
-        
-        for(int i=1; i<sizes.length; i++) {
-            int max = Math.max(sizes[i][0], sizes[i][1]);
-            int minMax = Math.min(sizes[i][0], sizes[i][1]);
-            
-            if(maxWidth < max) {
-                maxWidth = max;
-            }
-            
-            if(maxHeight < minMax) {
-                maxHeight = minMax;
-            }
-        }
-        
-        int answer = maxWidth * maxHeight;
-        
-        return answer;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        int maxwidth = 0;
-        int maxheight = 0;
+    public int solution(int[][] sizes) {
+        int maxW = 0;
+        int maxH = 0;
         
         for(int i=0; i<sizes.length; i++) {
-            int max = Math.max(sizes[i][0], sizes[i][1]);
-            int min = Math.min(sizes[i][0], sizes[i][1]);
+            int[] arr = sizes[i];
             
-            maxwidth = Math.max(maxwidth, max);
-            maxheight = Math.max(maxheight, min);
+            int w = arr[0];
+            int h = arr[1];
             
+            if(w > h) {
+                maxW = Math.max(maxW, w);
+                maxH = Math.max(maxH, h);
+            }
+            else {
+                maxW = Math.max(maxW, h);
+                maxH = Math.max(maxH, w);
+            }
         }
         
-        return maxwidth*maxheight;
-        
-        */
-    } 
+        return maxW * maxH;
+    }
 }
