8130ef5f9c9e05d020f6f6bfb5736e5f8bfe8443
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-13 23:29:12 +0900

    [level 3] Title: 여행경로, Time: 0.51 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,36 +1,39 @@
 import java.util.*;
-
-//복습
 class Solution {
-    
     public String[] solution(String[][] tickets) {
         Map<String, PriorityQueue<String>> map = new HashMap<>();
-        LinkedList<String> lq = new LinkedList<>(); 
         
-        for(String[] arr : tickets) {
-            String fr = arr[0];
-            String to = arr[1];
+        for(int i=0; i<tickets.length; i++) {
+            String[] str = tickets[i];
+            String from = str[0];
+            String to = str[1];
             
-            if(map.get(fr) == null) {
-                map.put(fr, new PriorityQueue<>());
-                map.get(fr).add(to);
+            if(map.get(from) == null) {
+                map.put(from, new PriorityQueue<>());
+                map.get(from).offer(to);
             }
-            else map.get(fr).add(to);
+            else map.get(from).offer(to);    
+        }
+        LinkedList<String> list = new LinkedList<>();
+        dfs("ICN", map, list);  
+        String[] str = new String[list.size()];
+        int cnt = 0;
+        for(String air : list) {
+            str[cnt++] = air;
         }
         
-        dfs("ICN", map, lq);
-        return lq.toArray(new String[0]);
-        
-        
+        return str;
     }
-    public void dfs(String airport, Map<String, PriorityQueue<String>> map, LinkedList<String> lq) {
-        PriorityQueue<String> que = map.get(airport);
+    
+    static void dfs(String airport, Map<String, PriorityQueue<String>> map, LinkedList<String> list) {
         
-        while(que != null && !que.isEmpty()) {
-            String airport2 = que.poll();
-            dfs(airport2, map, lq);
+        PriorityQueue<String> q = map.get(airport);
+        
+        while(q != null && !q.isEmpty()) {
+            String air = q.poll();
+            dfs(air, map, list);
         }
         
-        lq.addFirst(airport);
+        list.addFirst(airport);
     }
 }
