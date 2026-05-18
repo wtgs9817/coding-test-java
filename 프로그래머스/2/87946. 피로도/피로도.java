100917ad759081f4e8d482de3fc7d7d98f4c5dec
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-18 22:37:47 +0900

    [level 2] Title: 피로도, Time: 1.58 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,173 +1,21 @@
-//첫번째 풀이 재귀 조금 헷갈림. 하기 전에 좀 더 보고 다시 풀어보면 좋을듯
-//두번째 풀이
-//세번째 풀이
-// 네번째 풀이
 class Solution {
-    static boolean[] visited;
-    static int cnt = 0;
-    
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k,dungeons, cnt);
-        
-        return cnt;
-    
+        boolean[] visited = new boolean[dungeons.length];
+        return dfs(k, dungeons, visited, 0);
     }
     
-    public static void dfs(int k, int[][] dungeons, int count) {
-        
-        cnt = Math.max(cnt, count);
+    static int dfs(int piro, int[][] dungeons, boolean[] visited, int count) {
+        int max = count;
         
         for(int i=0; i<dungeons.length; i++) {
-            int cost = dungeons[i][1];
-            int required = dungeons[i][0];
-            
-            if(!visited[i] && k >= required) {
+            if(!visited[i] && piro >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(k-cost, dungeons, count+1);
-                visited[i] = false;
-            }  
-        }
-        
-        
-        
-        
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    int maxCount = 0;
-    boolean[] visited;
-    
-    
-    public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons, maxCount);
-        
-        return maxCount;
-
-    }
-    
-    public void dfs(int k, int[][] dungeons, int count) {
-        maxCount = Math.max(maxCount, count);
-        
-        for(int i=0; i< dungeons.length; i++) {
-            int min = dungeons[i][0];
-            int cost = dungeons[i][1];
-            
-            if(min <= k && visited[i] != true ) {
-                visited[i] = true;
-                dfs(k - cost, dungeons, count+1);
-                visited[i] = false;
+                int rmax = dfs(piro - dungeons[i][1], dungeons, visited, count+1);
+                max = Math.max(rmax, max);
+                
+                visited[i] = false; 
             }
         }
+        return max;
     }
-    
-    
-    
-    
-    
-    */
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    int maxCount = 0;
-    boolean[] visited;
-    
-    
-    public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0);
-        return maxCount;
-        
-        
-    }
-    
-    
-    public void dfs(int k, int[][] dungeons, int count) {
-        this.maxCount = Math.max(this.maxCount, count);
-        
-        for(int i=0; i<dungeons.length; i++) {
-            int required = dungeons[i][0];
-            int cost = dungeons[i][1];
-            
-            if(k >= required && !visited[i]) {
-                visited[i] = true;
-                dfs(k-cost, dungeons, count+1);
-                visited[i] = false;
-            }
-            
-        }
-    }
-    
-    
-    */
-        
-    /*    
-    int maxCount = 0;
-    boolean[] visited;
-    
-    public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons,  maxCount);
-        return maxCount;
-        
-        
-    }
-    
-    public void dfs(int k, int[][] dungeons, int count) {
-        this.maxCount = Math.max(this.maxCount, count);
-        
-        for(int i=0; i<dungeons.length; i++) {
-            int required = dungeons[i][0];
-            int cost = dungeons[i][1];
-            
-            if(k >= required && !this.visited[i]) {
-                visited[i] = true;
-                dfs(k - cost, dungeons, count+1);
-                visited[i] = false;
-            }   
-        }  
-    }
-    
-    
-    
-    
-    
-    */
-    
-        
-    
 }
