f1a37a160351e84e9b2b90b16b94dbb5f2c6bf2e
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-18 21:50:16 +0900

    [level 2] Title: 소수 찾기, Time: 12.58 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,36 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(String numbers) {
-        Set<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<>();
         boolean[] visited = new boolean[numbers.length()];
-        dfs(numbers, visited, "", set);
+        dfs(numbers, set, "", visited);
         
         return set.size();
     }
     
-    void dfs(String numbers, boolean[] visited, String str, Set<Integer> set) {
+    static void dfs(String numbers, HashSet<Integer> set, String str, boolean[] visited) {
         if(!str.isEmpty()) {
             int n = Integer.parseInt(str);
             if(isPrime(n)) set.add(n);
         }
-        
+
         for(int i=0; i<numbers.length(); i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(numbers, visited, str + numbers.charAt(i), set);
+                dfs(numbers, set, str+numbers.charAt(i), visited);
                 visited[i] = false;
             }
         }
         
+        
     }
     
-    boolean isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) return false; 
+    static boolean isPrime(int num) {
+        if(num <= 1) return false;
+        if(num == 2) return true;
+        if(num%2 == 0) return false;
+        
+        for(int i=3; i*i <= num; i+=2) {
+            if(num%i==0) return false;
         }
         return true;
     }
