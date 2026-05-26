d2859f818ddbe057b7a55fcfd574783c2be5c010
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-26 22:01:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.78 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,48 @@
+//복습
 import java.util.*;
 class Solution {
-    
-    //전력망을 2개로 분할 두 전력망 모두 갖게 되는 송전탑의 개수를 최대한 비슷하게 맞추기
-    
-    
     public int solution(int n, int[][] wires) {
-       ArrayList<Integer>[] list = new ArrayList[n+1];
-        int result = Integer.MAX_VALUE;
+        List<Integer>[] list = new ArrayList[n+1];
         
         for(int i=1; i<=n; i++) {
             list[i] = new ArrayList<>();
         }
         
-        for(int[] nums : wires) {
-            int a = nums[0];
-            int b = nums[1];
+        for(int[] nn : wires) {
+            int a = nn[0];
+            int b = nn[1];
             
             list[a].add(b);
             list[b].add(a);
         }
+        int result = Integer.MAX_VALUE;
         
-        for(int[] nums : wires) {
-            int a = nums[0];
-            int b = nums[1];
+        ArrayDeque<Integer> que = new ArrayDeque<>(); 
+        for(int[] nn : wires) {
+            int a = nn[0];
+            int b = nn[1];
+            int cnt = 1;
+            que.offer(a);
+            boolean[] visited = new boolean[n+1];
+            visited[a] = true;
             
-            int count = bfs(a, b, 0, n, list);
-            int count2 = n - count;
-            result = Math.min(Math.abs(count-count2), result);
+            while(!que.isEmpty()) {
+                int n1 = que.poll();    
+                
+                if(n1 == b) continue;
+                
+                for(int num : list[n1]) {
+                    if(num != b && !visited[num]) {
+                        cnt++;
+                        visited[num] = true;
+                        que.offer(num);
+                    }
+                } 
+            }
+            
+            result = Math.min(result, (Math.abs((n-cnt) - cnt)));
         }
         
         return result;
-    }
-    
-    static int bfs(int s, int e, int count, int n, ArrayList<Integer>[] list) {
-        ArrayDeque<Integer> que = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        que.offer(s);
-        count++;
-        visited[s] = true;
-        
-        while(!que.isEmpty()) {
-            int n1 = que.poll();
-            
-            for(int num : list[n1]) {
-                if(num == e) continue;
-                
-                if(!visited[num]) {
-                    visited[num] = true;
-                    que.offer(num);
-                    count++;
-                } 
-            }
-        }
-        return count;
     }
 }
