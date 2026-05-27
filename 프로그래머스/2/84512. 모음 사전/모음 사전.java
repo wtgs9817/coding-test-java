aebd83117351fcce9dec080d2cd1d9673757127c
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-05-27 22:19:25 +0900

    [level 2] Title: 모음 사전, Time: 4.12 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,22 +1,26 @@
+//복습
 class Solution {
-    String[] w = {"A","E","I","O","U"};
     int count = 0;
-    int result = 0;
+    boolean flag = false;
+    String[] str = {"A","E","I","O","U"};
     public int solution(String word) {
         dfs("", word);
-        return result;
+        return count;
     }
     
-    void dfs(String str, String word) {
-        if(str.equals(word)) {
-            result = count;
-        }
-        if(str.length() == 5) return;
+    public void dfs(String s, String w) {
+        if(flag) return;
         
-        for(int i=0; i<5; i++) {
+        if(!s.isEmpty()) {
             count++;
-            dfs(str + w[i], word);
+            
+            if(s.equals(w)) {
+                flag = true;
+                return;
+            }
+            if(s.length() == 5) return;
         }
-        if(result != 0) return;
-    }
+        for(int i=0; i<str.length; i++) dfs(s + str[i], w);
+    }  
+    
 }
