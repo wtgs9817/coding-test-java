39f41d5d49be1f96a628d9efe01c76d6387b41fb
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-06-08 00:11:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.77 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,24 @@
+//복습
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
+        HashMap<String, Integer> map = new HashMap<>();
         String answer = "";
-        for(int i=0; i<completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                answer = participant[i];
+        
+        for(int i=0; i<participant.length; i++){
+            String key = participant[i];
+            if(map.containsKey(key)) map.put(key, map.get(key) + 1);
+            else map.put(key, 1);
+        }
+        
+        for(String key : completion) if(map.containsKey(key)) map.put(key, map.get(key) -1);
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
                 break;
             }
         }
-        if(answer.equals("")) answer = participant[participant.length-1];
-        
+            
         return answer;
     }
 }
