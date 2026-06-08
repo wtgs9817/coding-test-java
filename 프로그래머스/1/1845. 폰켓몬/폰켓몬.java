4fb781b2e9649098dc491ed130056340db2c1a4a
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-06-08 21:31:10 +0900

    [level 1] Title: 폰켓몬, Time: 1.96 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,45 +1,14 @@
-//첫번째 풀이
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
+        int a = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) set.add(nums[i]);
         
-        for(int i=0; i<nums.length; i++) {
-            set.add(nums[i]);
-            
-            if(set.size() == nums.length/2) {
-                break;
-            }
-        }
-        
-        return set.size();
-        
-        
-        
-        /* 방법 2
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(nums[0]);
-        
-        
-        
-        for(int i=0; i<nums.length; i++) {
-            
-            if(!list.contains(nums[i])) {
-                list.add(nums[i]);
-            }
-            
-            if(list.size() == nums.length/2) {
-                break;
-            }
-        }
-        
-        
-        return list.size();
-        */
-        
-        
-        
-        
-        
+        int answer = 0;
+        if(set.size() >= a) answer = a;
+        else answer = set.size();
+         
+        return answer;
     }
 }
