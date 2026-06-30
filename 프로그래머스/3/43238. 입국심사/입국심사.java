2a1f3a377b7da05d8e8f2f2fa405b320a4c39080
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-06-30 21:45:36 +0900

    [level 3] Title: 입국심사, Time: 37.53 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,29 @@
 import java.util.*;
+//복습
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = Integer.MAX_VALUE;
+        long answer = 0;
         Arrays.sort(times);
         
         long left = 1;
-        long right = (long) times[times.length - 1] * n;
-        
+        long right = (long) times[times.length-1] * n;
         
         while(left <= right) {
-            long pe = 0;
             long mid = (left + right) / 2;
+            long p = 0;
             
             for(int time : times) {
-                pe += mid / time;
-                if(pe >= n) break;
+                p +=  (long) mid / time;
+                if(p >= n) break;
             }
             
-            if(pe >= n) {
+            if(p >= n) {
                 answer = mid;
                 right = mid - 1;
             }
             else left = mid + 1;
         }
+        
         return answer;
     }
 }
