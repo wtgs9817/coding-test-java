9fd83f88b6bc9aafab0521db2c105d18ce1f5ba2
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-07-04 14:25:06 +0900

    [level 3] Title: 여행경로, Time: 0.36 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,39 +1,36 @@
+//복습
+
 import java.util.*;
 class Solution {
-    public String[] solution(String[][] tickets) {
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
+    static Map<String, PriorityQueue<String>> ap = new HashMap<>();
+    static List<String> result = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets)    {
         
-        for(int i=0; i<tickets.length; i++) {
-            String[] str = tickets[i];
-            String from = str[0];
-            String to = str[1];
+        for(String[] s : tickets) {
+            if(!ap.containsKey(s[0])) ap.put(s[0], new PriorityQueue<>());
             
-            if(map.get(from) == null) {
-                map.put(from, new PriorityQueue<>());
-                map.get(from).offer(to);
-            }
-            else map.get(from).offer(to);    
-        }
-        LinkedList<String> list = new LinkedList<>();
-        dfs("ICN", map, list);  
-        String[] str = new String[list.size()];
-        int cnt = 0;
-        for(String air : list) {
-            str[cnt++] = air;
+            ap.get(s[0]).offer(s[1]);
         }
         
-        return str;
+        dfs("ICN");
+        Collections.reverse(result);
+        String[] rs = new String[result.size()];
+        int cnt = 0;
+        
+        for(String s : result) rs[cnt++] = s;
+        
+        return rs;
     }
     
-    static void dfs(String airport, Map<String, PriorityQueue<String>> map, LinkedList<String> list) {
+    static void dfs(String airport) {
+        PriorityQueue<String> fap = ap.get(airport);
         
-        PriorityQueue<String> q = map.get(airport);
-        
-        while(q != null && !q.isEmpty()) {
-            String air = q.poll();
-            dfs(air, map, list);
+        while(fap != null && !fap.isEmpty()) {
+            String a = fap.poll();
+            dfs(a);
         }
         
-        list.addFirst(airport);
+        result.add(airport);
     }
 }
