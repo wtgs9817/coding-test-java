dd16fdf645d29ea1b4b746291d6ca7d65aa156db
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-07-13 16:20:20 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.63 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,48 +1,54 @@
-//복습
 import java.util.*;
+//다시 풀어보기
+
 class Solution {
+    static List<Integer>[] list;
     public int solution(int n, int[][] wires) {
-        List<Integer>[] list = new ArrayList[n+1];
+        list = new ArrayList[n+1];
+        int result = Integer.MAX_VALUE;
         
-        for(int i=1; i<=n; i++) {
-            list[i] = new ArrayList<>();
-        }
-        
-        for(int[] nn : wires) {
-            int a = nn[0];
-            int b = nn[1];
+        for(int i=1; i<=n; i++) list[i] = new ArrayList<>();
+        for(int i=0; i<wires.length; i++) {
+            int[] arr = wires[i];
+            int a = arr[0];
+            int b = arr[1];
             
             list[a].add(b);
             list[b].add(a);
         }
-        int result = Integer.MAX_VALUE;
         
-        ArrayDeque<Integer> que = new ArrayDeque<>(); 
-        for(int[] nn : wires) {
-            int a = nn[0];
-            int b = nn[1];
-            int cnt = 1;
-            que.offer(a);
-            boolean[] visited = new boolean[n+1];
-            visited[a] = true;
+        for(int i=0; i<wires.length; i++) {
+            int[] arr = wires[i];
             
-            while(!que.isEmpty()) {
-                int n1 = que.poll();    
-                
-                if(n1 == b) continue;
-                
-                for(int num : list[n1]) {
-                    if(num != b && !visited[num]) {
-                        cnt++;
-                        visited[num] = true;
-                        que.offer(num);
-                    }
-                } 
-            }
-            
-            result = Math.min(result, (Math.abs((n-cnt) - cnt)));
+            int count = bfs(n, wires, arr, i);
+            result = Math.min(count, result);
         }
         
-        return result;
+        return result;     
+    }
+    
+    static int bfs(int n, int[][] wires, int[] arr, int node) {
+        int count = 1;
+        Deque<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        int a = arr[0];
+        int b = arr[1];
+        
+        q.offer(b);
+        visited[b] = true;
+        
+        while(!q.isEmpty()) {
+            int aa = q.poll();
+            
+            for(int aaa : list[aa]) {
+                if(aaa != a && !visited[aaa]) {
+                    q.offer(aaa);
+                    visited[aaa] = true;
+                    count++;
+                } 
+            }
+        }
+        
+        return Math.abs((n - count) - count);
     }
 }
