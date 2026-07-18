c6bf286f2b4ff3d9d334ce27c57f8ab2ad13f23c
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-07-18 15:35:20 +0900

    [level 2] Title: 양궁대회, Time: 0.57 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,17 +1,21 @@
 /*
 1 ≤ n ≤ 10
-라이언이 가장 큰 점수 차이로 우승할 수 있는 방법이 여러 가지 일 경우, 가장 낮은 점수를 더 많이 맞힌 경우를 return
+info의 길이 = 11
+0 ≤ info의 원소 ≤ n
+info의 i번째 원소는 과녁의 10 - i 점을 맞힌 화살 개수
+
 */
+
+//복습
 class Solution {
     static int[] result;
-    static int maxScore;
+    static int max;
     
     public int[] solution(int n, int[] info) {
-        // info -> apeach
         result = new int[]{-1};
-        maxScore = 0;
-        int[] lion = new int[11];
+        max = 0;
         
+        int[] lion = new int[11];
         dfs(0, n, info, lion);
         
         return result;
@@ -19,41 +23,46 @@ class Solution {
     
     static void dfs(int depth, int arrow, int[] apeach, int[] lion) {
         if(depth == 11) {
-            if(arrow > 0) lion[10] += arrow;
+            if(arrow > 0) lion[10]+=arrow;
             
-            int a = 0;
-            int l = 0;
+            int lionScore = 0;
+            int apeachScore = 0;
             
             for(int i=0; i<=10; i++) {
                 if(apeach[i] == 0 && lion[i] == 0) continue;
-                if(apeach[i] < lion[i]) l += (10 - i);    
-                else a+= (10 - i);
+                
+                if(apeach[i] < lion[i]) lionScore+=(10 - i);    
+                else apeachScore+=(10 - i);
             }
             
-            int score = l - a;
+            int total = lionScore - apeachScore;
             
-            if(score > 0) {
-                if(maxScore < score) {  //라이언 값의 최대가 아니라 l-a 가 최대가 되는 값을 구해야 함.
+            if(total > 0) {
+                if(max < total) {
+                    max = total;
                     result = lion.clone();
-                    maxScore = score;
                 }
-                else if(maxScore == score) {
+                else if(max == total) {
                     for(int i=10; i>=0; i--) {
-                        if(result[i] < lion[i]) {
+                        if(lion[i] == result[i]) continue;
+                        if(lion[i] > result[i]) {
                             result = lion.clone();
                             break;
                         }
-                        else if(result[i] > lion[i]) break;
+                        else break;
                     }
                 }
             }
-            if(arrow > 0) lion[10] -= arrow;
+            
+            if(arrow > 0) lion[10]-=arrow;
             return;
         }
         
-        if(arrow > apeach[depth]) {
+        if(apeach[depth] < arrow) {
             lion[depth] = apeach[depth] + 1;
-            dfs(depth+1, arrow - lion[depth], apeach, lion);
+            arrow -= (lion[depth]);
+            dfs(depth+1, arrow, apeach, lion);
+            arrow +=(lion[depth]);
             lion[depth] = 0;
         }
         dfs(depth+1, arrow, apeach, lion);
