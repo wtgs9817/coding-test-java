f81d620655b11227880bdf039cda82942b4eac09
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-07-20 22:10:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.39 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,15 +1,22 @@
-import java.util.*;
-//다시 풀어보기
+//복습
+/*
+n은 2 이상 100 이하인 자연수
+wires는 길이가 n-1인 정수형 2차원 배열 --> 트리
+wires의 각 원소는 [v1, v2] 2개의 자연수로 이루어져 있으며, 이는 전력망의 v1번 송전탑과 v2번 송전탑이 전선으로 연결되어 있다는 것을 의미
+*/
 
+import java.util.*;
 class Solution {
     static List<Integer>[] list;
+    static int result;
+    
     public int solution(int n, int[][] wires) {
         list = new ArrayList[n+1];
-        int result = Integer.MAX_VALUE;
+        result = Integer.MAX_VALUE;
+        int len = wires.length;
         
         for(int i=1; i<=n; i++) list[i] = new ArrayList<>();
-        for(int i=0; i<wires.length; i++) {
-            int[] arr = wires[i];
+        for(int[] arr : wires) {
             int a = arr[0];
             int b = arr[1];
             
@@ -17,38 +24,32 @@ class Solution {
             list[b].add(a);
         }
         
-        for(int i=0; i<wires.length; i++) {
-            int[] arr = wires[i];
+        for(int[] arr : wires) {
+            Deque<Integer> que = new ArrayDeque<>();
+            boolean[] visited = new boolean[n+1];
+            int a = arr[0];
+            int b = arr[1];
             
-            int count = bfs(n, wires, arr, i);
-            result = Math.min(count, result);
-        }
-        
-        return result;     
-    }
-    
-    static int bfs(int n, int[][] wires, int[] arr, int node) {
-        int count = 1;
-        Deque<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        int a = arr[0];
-        int b = arr[1];
-        
-        q.offer(b);
-        visited[b] = true;
-        
-        while(!q.isEmpty()) {
-            int aa = q.poll();
+            que.offer(b);
+            visited[b] = true;
+            int cnt = 1;
             
-            for(int aaa : list[aa]) {
-                if(aaa != a && !visited[aaa]) {
-                    q.offer(aaa);
-                    visited[aaa] = true;
-                    count++;
-                } 
+            while(!que.isEmpty()) {
+                int num = que.poll();
+                
+                for(int n1 : list[num]) {
+                    if(n1 != a && !visited[n1]) {
+                        visited[n1] = true;
+                        que.offer(n1);
+                        cnt++;
+                    }
+                }
             }
+            
+            int w = Math.abs((n - cnt) - cnt);
+            result = Math.min(result, w);
         }
         
-        return Math.abs((n - count) - count);
-    }
+        return result;      
+    }      
 }
