0f10cdd30d6e75ca04ecc0efc7c7fb161e8a9129
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-07-25 15:32:44 +0900

    [level 3] Title: 양과 늑대, Time: 3.06 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,53 +1,42 @@
 import java.util.*;
 /*
 2 ≤ info의 길이 ≤ 17
-info의 원소는 0 또는 1 
-info[i]는 i번 노드에 있는 양 또는 늑대(0은 양, 1은 늑대)
-info[0]의 값은 항상 0입니다. 즉, 0번 노드(루트 노드)에는 항상 양
-
+info의 원소는 0 또는 1  0 -> 양 / 1 -> 늑대
 edges의 세로(행) 길이 = info의 길이 - 1
 edges의 가로(열) 길이 = 2
 edges의 각 행은 [부모 노드 번호, 자식 노드 번호] 형태로, 서로 연결된 두 노드를 나타냄
+당신이 모은 양의 수보다 늑대의 수가 같거나 더 많아지면 바로 모든 양을 잡아먹어 버림
 */
 class Solution {
     static List<Integer>[] tree;
     static int maxSheep;
+    static List<Integer> nextNodes;
     public int solution(int[] info, int[][] edges) {
+        tree = new ArrayList[info.length];
         maxSheep = 0;
-        int len = info.length;
-        int eLen = edges.length;
-        tree = new ArrayList[len];
         
-        for(int i=0; i<len; i++) tree[i] = new ArrayList<>();
+        for(int i=0; i<info.length; i++) tree[i] = new ArrayList<>();
+        for(int[] arr : edges) tree[arr[0]].add(arr[1]);
         
-        for(int[] arr : edges) {
-            int a = arr[0];
-            int b = arr[1];
-            
-            tree[a].add(b);
-        }
+        nextNodes = new ArrayList<>(tree[0]);
+        dfs(1, 0, nextNodes, info);
         
-        List<Integer> save = new ArrayList<>();
-        save.addAll(tree[0]);
-        
-        dfs(1, 0, info, save);
         return maxSheep;
-        
     }
-    public void dfs(int sheep, int wolf, int[] info ,List<Integer> save) {
+    
+    static void dfs(int sheep, int wolf, List<Integer> nextNode, int[] info) {
         maxSheep = Math.max(sheep, maxSheep);
         
-        for(int i=0; i<save.size(); i++) {
-            int a = save.get(i);
+        for(int i=0; i<nextNode.size(); i++) {
+            int a = nextNode.get(i);
             int isWolf = info[a];
             
-            if(sheep <= isWolf + wolf) continue;
+            if(sheep <= wolf + isWolf) continue;
             
-            List<Integer> back = new ArrayList<>(save);
-            back.remove(i);
-            back.addAll(tree[a]);
-
-            dfs(sheep + (1- isWolf), wolf + isWolf, info, back); 
+            List<Integer> next = new ArrayList<>(nextNode);
+            next.remove(i);
+            next.addAll(tree[a]);
+            dfs(sheep + (1 - isWolf), wolf + isWolf, next, info);
         }
     }
 }
