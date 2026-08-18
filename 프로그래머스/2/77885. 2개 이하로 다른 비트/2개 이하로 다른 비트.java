44c4f4fcb0e6971073190779e28e1d7fa3dca6db
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-08-18 15:21:54 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 5.46 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,20 +1,18 @@
-/*
-1 ≤ numbers의 길이 ≤ 100,000
-0 ≤ numbers의 모든 수 ≤ 10^15
-x보다 크고 x와 비트가 1~2개 다른 수들 중에서 제일 작은 수
-*/
+//복습
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] arr = new long[numbers.length];
+        long[] result = new long[numbers.length];
         int cnt = 0;
-        for(long num : numbers) {
-            if(num % 2 == 0) arr[cnt++] = num+1;
+        
+        for(long n : numbers) {
+            if(n % 2 == 0) result[cnt++] = n+1;
             else {
-                int k = 0;
-                while((num & (1L << k)) != 0) k++;
-                arr[cnt++] = (num | 1L << k) & ~(1L << k-1);
-            }
+                int idx = -1;
+                int num = 0;
+                while((n & (1L << num)) != 0) num++;
+                result[cnt++] = (n | (1L << num)) & ~(1L << num-1);
+            }  
         }
-        return arr;
+        return result;
     }
 }
