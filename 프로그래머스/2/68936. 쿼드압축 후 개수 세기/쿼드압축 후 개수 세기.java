9c31938d455e2fd71a04b3ba3abdcf5cc76b0292
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-08-20 14:30:16 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.98 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,17 +1,18 @@
 /*
-arr의 행의 개수는 1 이상 1024 이하(2의 거듭 제곱수 형태)
- 즉, arr의 행의 개수는 1, 2, 4, 8, ..., 1024 중 하나
- arr의 각 행의 길이는 arr의 행의 개수와 같음 즉 arr은 정사각형 배열
- arr의 각 행에 있는 모든 값은 0 또는 1 
+arr의 행의 개수는 1 이상 1024 이하이며, 2의 거듭 제곱수 형태
+즉, arr의 행의 개수는 1, 2, 4, 8, ..., 1024 중 하나
+arr의 각 행의 길이는 arr의 행의 개수와 같습니다. 즉, arr은 정사각형 배열
+arr의 각 행에 있는 모든 값은 0 또는 1 
 
-다시 풀어봐야 할듯
+
+복습 
 */
 class Solution {
     static int[] result;
     public int[] solution(int[][] arr) {
         result = new int[2];
+        div(0, 0, arr.length, arr);
         
-        div(0,0, arr.length, arr);
         return result;
     }
     
@@ -22,11 +23,12 @@ class Solution {
         }
         
         int half = size/2;
-        div(r,c, half, arr);
+        div(r, c, half, arr);
         div(r, c + half, half, arr);
         div(r + half, c, half, arr);
         div(r + half, c + half, half, arr);
     }
+    
     
     static boolean isSame(int r, int c, int size, int[][] arr) {
         int n = arr[r][c];
@@ -34,7 +36,7 @@ class Solution {
         for(int i=r; i< r + size; i++) {
             for(int k=c; k< c + size; k++) {
                 if(n != arr[i][k]) return false;
-            }
+            }     
         }
         return true;
     }
