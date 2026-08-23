ae987afb4951bb0f359797abdc0ac95bb4944b31
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-08-23 14:25:27 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.08 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,11 +1,13 @@
+import java.util.*;
 /*
 players의 길이 = 24
 0 ≤ players의 원소 ≤ 1,000
-players[i]는 i시 ~ i+1시 사이의 게임 이용자의 수를 나타냄
-1 ≤ m ≤ 1,000 -> 서버 한 대로 감당할 수 있는 인원
-1 ≤ k ≤ 24 -> 서버 운영 가능 시간
+players[i]는 i시 ~ i+1시 사이의 게임 이용자의 수
+1 ≤ m ≤ 1,000 --> m명 늘어날 때마다 서버++
+1 ≤ k ≤ 24 --> 서버 유지 시간
+
+복습
 */
-import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
         ArrayDeque<int[]> que = new ArrayDeque<>();
@@ -15,16 +17,14 @@ class Solution {
         for(int i=0; i<players.length; i++) {
             if(!que.isEmpty() && que.peek()[0] == i) {
                 int[] arr = que.poll();
-                server -= arr[1];
+                server-= arr[1];
             }
             
-            int s = players[i] / m;
-            
-            if(s > server) {
-               int ns = s - server;
+            if(players[i] / m > server) {
+                int ns = players[i]/m - server;
+                
                 server += ns;
                 result += ns;
-                
                 que.offer(new int[]{i+k, ns});
             }
         }
