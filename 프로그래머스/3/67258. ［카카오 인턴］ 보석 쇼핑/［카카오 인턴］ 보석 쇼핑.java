5646e070fbf18bdd3359ae41b617c4fb576d6abb
김명찬 <87848350+wtgs9817@users.noreply.github.com>
2026-08-25 14:19:28 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 27.18 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,46 +1,48 @@
 import java.util.*;
 /*
-gems 배열의 크기는 1 이상 100,000 이하 / gems 배열의 각 원소는 진열대에 나열된 보석
-gems 배열에는 1번 진열대부터 진열대 번호 순서대로 보석이름이 차례대로 저장
+gems 배열의 크기는 1 이상 100,000 이하
+gems 배열 -> 1번부터 순서대로 보석이름 저장
 gems 배열의 각 원소는 길이가 1 이상 10 이하인 알파벳 대문자로만 구성된 문자열
+
+복습
 */
 class Solution {
     public int[] solution(String[] gems) {
-        int[] result = new int[2];
         HashSet<String> set = new HashSet<>();
         int len = gems.length;
         int mlen = Integer.MAX_VALUE;
+        int[] result = new int[2];
         
         for(int i=0; i<len; i++) set.add(gems[i]);
-        
-        HashMap<String, Integer> map = new HashMap<>();
+         
         int start = 0;
         int end = 0;
         
+        HashMap<String, Integer> map = new HashMap<>();
+        
         while(true) {
             if(map.size() == set.size()) {
-                String a = gems[start];
-                
                 if(mlen > end - start) {
                     mlen = end - start;
-                                 
+                    
                     result[0] = start + 1;
-                    result[1] = end; 
+                    result[1] = end;
                 }
+                String str = gems[start];
+                if(map.get(str) - 1 > 0) map.put(str, map.get(str) - 1);
+                else map.remove(str);
                 
-                if(map.get(a) - 1 > 0) map.put(a, map.get(a) - 1);
-                else map.remove(a);
                 start++;
             }
             
             else if(end >= len) break;
             else {
-                if(!map.containsKey(gems[end])) map.put(gems[end], 1);
-                else map.put(gems[end], map.get(gems[end]) + 1);
-                
-                end++;
+                String str = gems[end++];
+                if(!map.containsKey(str)) map.put(str, 1);
+                else map.put(str, map.get(str) + 1);
             }
         }
+        
         return result;
     }
 }
